7600b439c51c86d38dcc13b36644bd68cc331ab8
Daddycool122 <akhileshramola.2000@gmail.com>
2025-05-19 05:27:42 +0530

    Gemini API

--- src/components/GptMovieSuggestions.jsx
+++ src/components/GptMovieSuggestions.jsx
@@ -3,26 +3,30 @@ import { useSelector } from 'react-redux'
 import MovieList from './MovieList';
 
 const GptMovieSuggestions = () => {
-
-  const {geminiMovies,geminiMovieNames} = useSelector((store) => store.gpt);
-  if(!geminiMovieNames)return null;
+  const { geminiMovies, geminiMovieNames } = useSelector((store) => store.gpt);
+  if (!geminiMovieNames) return null;
 
   return (
-    <div className='p-2 m-2 bg-black text-white'>
-      <div>
-        
-        <h1 className='text-2xl font-bold'>Movie Suggestions</h1>
-        
-        {geminiMovieNames.map((movie,index)=>{
-          return(
-            <MovieList key={movie} title={movie} movies={geminiMovies[index]} />
-          )
-          
-        })}
-        
+    <div className="p-2 sm:p-4 md:p-8  sm:m-4 bg-black text-white min-h-[30vh]">
+      <div className="max-w-7xl mx-auto w-full">
+        {geminiMovieNames.length > 0 && (
+          <h1 className="text-2xl sm:text-3xl md:text-4xl p-2 sm:p-4  text-center">
+            Movie Suggestions
+          </h1>
+        )}
+
+        <div className="flex flex-col gap-6">
+          {geminiMovieNames.map((movie, index) => (
+            <MovieList
+              key={movie}
+              title={movie}
+              movies={geminiMovies && geminiMovies[index] ? geminiMovies[index] : []}
+            />
+          ))}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default GptMovieSuggestions
